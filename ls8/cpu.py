8b7efdc54893751d6d17d530c62a51eb3d4d8bee
RichVI <richardwang2050@gmail.com>
2020-07-23 21:55:38 -0700

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,7 +5,12 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+ADD = 0b10100000
 MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
 
 
 class CPU:
@@ -15,7 +20,20 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[7] = 0xF4
         self.pc = 0
+        self.running = True
+        # set up branch table
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[ADD] = self.handle_ADD
+        self.branchtable[MUL] = self.handle_MUL
+        self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[POP] = self.handle_POP
+        self.branchtable[CALL] = self.handle_CALL
+        self.branchtable[RET] = self.handle_RET
 
     def ram_read(self, mar):  # accept Memory Address Register (MAR)
         return self.ram[mar]
@@ -27,18 +45,6 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
         try:
             with open(file_name) as file:
                 for line in file:
@@ -59,10 +65,77 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
-        elif op == MUL:
+        elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
+    def handle_HLT(self):
+        self.running = False
+
+    def handle_LDI(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.reg[operand_a] = operand_b
+
+    def handle_PRN(self):
+        reg_num = self.ram_read(self.pc + 1)
+        print(self.reg[reg_num])
+
+    def handle_ADD(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("ADD", operand_a, operand_b)
+
+    def handle_MUL(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", operand_a, operand_b)
+
+    def handle_PUSH(self):
+        # decrement the stack pointer
+        self.reg[7] -= 1
+        # get a value from the given register
+        register = self.ram_read(self.pc + 1)
+        value = self.reg[register]
+        # put the value at the stack address
+        sp = self.reg[7]
+        self.ram[sp] = value
+
+    def handle_POP(self):
+        # get the stack pointer address
+        sp = self.reg[7]
+        # get a value from the given register
+        register = self.ram_read(self.pc + 1)
+        # use stack pointer to get the value
+        value = self.ram[sp]
+        # put the value into given register
+        self.reg[register] = value
+        # increment stack pointer
+        self.reg[7] += 1
+
+    def handle_CALL(self):
+        # get the register number
+        reg = self.ram_read(self.pc + 1)
+        # get the address to jump to, from the register
+        address = self.reg[reg]
+        # push command after CALL onto the stack
+        return_address = self.pc + 2
+        # decrement stack pointer
+        self.reg[7] -= 1
+        sp = self.reg[7]
+        # put return address on the stack
+        self.ram[sp] = return_address
+        # look at register, jump to the address
+        self.pc = address
+
+    def handle_RET(self):
+        # pop the return address off the stack
+        sp = self.reg[7]
+        return_address = self.ram[sp]
+        self.reg[7] += 1
+        # go the return address: set the pc to return address
+        self.pc = return_address
 
     def trace(self):
         """
@@ -85,22 +158,14 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
+        while self.running:
             ir = self.ram_read(self.pc)  # Instruction Register
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            if ir == HLT:
-                running = False
-            elif ir == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif ir == PRN:
-                reg_num = self.ram_read(self.pc + 1)
-                print(self.reg[reg_num])
-                self.pc += 2
-            elif ir == MUL:
-                self.alu(ir, operand_a, operand_b)
-                self.pc += 3
-            else:
+            value = ir
+            op_count = value >> 6
+            ir_length = 1 + op_count
+            self.branchtable[ir]()
+            if ir == 0 or None:
                 print(f"Unknown Instruction: {ir}")
+                sys.exit()
+            if ir != CALL and ir != RET:
+                self.pc += ir_length
